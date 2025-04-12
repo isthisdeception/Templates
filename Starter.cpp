346f9926d3d74fe2b68005a82c2ae766090a657b
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2025-04-12 23:57:33 +0600

    Starter

---
+++ Starter.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// debug
+#define dbg(a) cout << #a << " = " << a << '\n';
+
+// ordered set
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds; 
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	
+	
+	return 0; 
+}
