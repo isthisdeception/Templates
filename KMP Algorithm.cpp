17f440a93b28b22c01bbc87de274eb1a3e9a2922
iamassaduzzaman <md.assaduzzaman99@gmail.com>
2025-08-12 22:56:46 +0600

    KMP Algorithm

---
+++ KMP Algorithm.cpp
@@ -0,0 +1,13 @@
+// kmp O(n)
+vector<int> kmp(const string &s) {
+    int n = s.size();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i - 1];
+        while (j > 0 and s[i] != s[j])
+            j = pi[j - 1];
+        if (s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+    return pi;
+}
